3c69576ceb83befdbda66e638ae5c60f13bc965c
Mikhail Charniak <60339865+CharniakMikhail@users.noreply.github.com>
2020-02-13 17:09:10 +0300

    refactoring image viewer component

--- src/modules/ImageViewer/components/ImageViewer.tsx
+++ src/modules/ImageViewer/components/ImageViewer.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import StyledImage from '../styled/StyledImage';
 import StyledImageView from '../styled/StyledImageView';
-import {createIdGenerator} from '../../../core/utils/idGenerator';
-import {IImage} from '../../imageDraw/interface/IImage';
+import { createIdGenerator } from '../../../core/utils/idGenerator';
+import { IImage } from '../../imageDraw/interface/IImage';
 
 interface Props {
     imageArray: IImage[],
 }
 
 const idGenerator = createIdGenerator();
-
-const mapImage = (imageArray: IImage[]) => imageArray.map(item => <StyledImage key={idGenerator.next().value} src={item.imageURL} alt="..."/>);
-
+    
 const ImageViewer: React.FC<Props> = ({imageArray}) => {
+    const mapImage = useCallback((image: IImage) => (
+        <StyledImage key={idGenerator.next().value} src={image.imageURL} alt="..."/>
+    ));
+
     return (
         <StyledImageView>
-            {imageArray && mapImage(imageArray)}
+            {imageArray.map(mapImage)}
         </StyledImageView>
     );
+};
+
+ImageViewer.defaultProps = {
+    imageArray: [],
 };
 
 export default ImageViewer;
